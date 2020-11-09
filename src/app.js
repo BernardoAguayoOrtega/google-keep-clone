efe15e0cfd56fb8a8271760b2b71b6ae8a46d190
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 11:53:01 -0600

    add has note validation

--- src/app.js
+++ src/app.js
@@ -1,52 +1,54 @@
 class App {
-  constructor() {
-    this.$form = document.querySelector("#form");
-    this.$noteTitle = document.querySelector("#note-title");
-    this.$noteText = document.querySelector("#note-text");
-    this.$formButtons = document.querySelector("#form-buttons");
+	constructor() {
+		this.$form = document.querySelector('#form');
+		this.$noteTitle = document.querySelector('#note-title');
+		this.$noteText = document.querySelector('#note-text');
+		this.$formButtons = document.querySelector('#form-buttons');
 
-    this.addEventListeners();
-  }
+		this.addEventListeners();
+	}
 
-  addEventListeners() {
-    document.body.addEventListener("click", event => {
-      this.handleFormClick(event);
-    });
-    
-    this.$form.addEventListener('submit', event => {
-      event.preventDefault(); 
-      
-      const note = {
-        title: this.$noteTitle.value, 
-        text: this.$noteTitle.value
-      }
+	addEventListeners() {
+		document.body.addEventListener('click', (event) => {
+			this.handleFormClick(event);
+		});
 
-      console.log(note)
-      this.closeForm()
-    });
-  }
+		this.$form.addEventListener('submit', (event) => {
+			event.preventDefault();
 
-  handleFormClick(event) {
-    const isFormClicked = this.$form.contains(event.target);
+			const note = {
+				title: this.$noteTitle.value,
+				text: this.$noteTitle.value,
+			};
 
-    if (isFormClicked) {
-      this.openForm();
-    } else {
-      this.closeForm();
-    }
-  }
+			const hasNote = note.text || note.title ? true : false;
 
-  openForm() {
-    this.$form.classList.add("form-open");
-    this.$noteTitle.style.display = "block";
-    this.$formButtons.style.display = "block";
-  }
+			console.log(note, hasNote);
+			this.closeForm();
+		});
+	}
 
-  closeForm() {
-    this.$form.classList.remove("form-open");
-    this.$noteTitle.style.display = "none";
-    this.$formButtons.style.display = "none";
-  }
+	handleFormClick(event) {
+		const isFormClicked = this.$form.contains(event.target);
+
+		if (isFormClicked) {
+			this.openForm();
+		} else {
+			this.closeForm();
+		}
+	}
+
+	openForm() {
+		this.$form.classList.add('form-open');
+		this.$noteTitle.style.display = 'block';
+		this.$formButtons.style.display = 'block';
+	}
+
+	closeForm() {
+		this.$form.classList.remove('form-open');
+		this.$noteTitle.style.display = 'none';
+		this.$formButtons.style.display = 'none';
+	}
 }
 
 new App();
