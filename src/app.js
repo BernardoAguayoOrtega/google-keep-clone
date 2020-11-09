312be081003e1bbc89b3440949bf7cc6ed60d0b5
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 11:09:23 -0600

    Update app file with initialize constructor

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 class App {
-  constructor() {
-    window.alert('app works!')  
-  }    
+	constructor() {
+		this.$form = document.querySelector('#form');
+
+		this.addEventListeners();
+	}
+
+	addEventListeners() {
+		document.body.addEventListener('click', (event) => {
+			this.handleFormClick(event);
+		});
+	}
+
+	handleFormClick(event) {
+		const ifFormClicked = this.$form.contains(event.target);
+
+		ifFormClicked ? '' : '';
+	}
 }
 
-new App()
+new App();
