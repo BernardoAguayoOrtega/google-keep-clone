188ce800f4deeb30c9797bbf64b5bf44ce431b8c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 11:20:24 -0600

    Fix bug with app.js file

--- src/app.js
+++ src/app.js
@@ -1,31 +1,29 @@
 class App {
-	constructor() {
-		this.$form = document.querySelector('#form');
-
+  constructor() {
+    this.$form = document.querySelector('#form');
     this.$noteTitle = document.querySelector('#note-title');
-
     this.$formButtons = document.querySelector('#form-buttons');
-
-		this.addEventListeners();
-	}
-
-	addEventListeners() {
-		document.body.addEventListener('click', (event) => {
-			this.handleFormClick(event);
-		});
-	}
-
-	handleFormClick(event) {
-		const ifFormClicked = this.$form.contains(event.target);
-
-		if (ifFormClicked) this.openForm;
-	}
-
-	openForm() {
-    this.$form.classList.add('form-open');
-    this.$noteTitle.style.display = 'block'
-    this.$formButtons.style.display = 'block'
-	}
+    
+    this.addEventListeners();
+  }
+  
+  addEventListeners() {
+    document.body.addEventListener('click', event => {
+      this.handleFormClick(event);
+    });
+  }
+  
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target); 
+    
+    isFormClicked ? this.openForm() : ""
+  }
+  
+  openForm() {
+    this.$form.classList.add('form-open');  
+    this.$noteTitle.style.display = 'block';
+    this.$formButtons.style.display = 'block';
+  }
 }
 
 new App();
