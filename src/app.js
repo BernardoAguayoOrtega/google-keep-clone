0aa9be947eb919d0a50d6f5a3bebdfd78665eef2
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 11:25:58 -0600

    make private the class variables

--- src/app.js
+++ src/app.js
@@ -1,29 +1,35 @@
 class App {
-  constructor() {
-    this.$form = document.querySelector('#form');
-    this.$noteTitle = document.querySelector('#note-title');
-    this.$formButtons = document.querySelector('#form-buttons');
-    
-    this.addEventListeners();
-  }
-  
-  addEventListeners() {
-    document.body.addEventListener('click', event => {
-      this.handleFormClick(event);
-    });
-  }
-  
-  handleFormClick(event) {
-    const isFormClicked = this.$form.contains(event.target); 
-    
-    isFormClicked ? this.openForm() : ""
-  }
-  
-  openForm() {
-    this.$form.classList.add('form-open');  
-    this.$noteTitle.style.display = 'block';
-    this.$formButtons.style.display = 'block';
-  }
+  /**private variables */
+	#$form;
+	#$noteTitle;
+	#$formButtons;
+
+	constructor() {
+		/** innit variables */
+		this.#$form = document.querySelector('#form');
+		this.#$noteTitle = document.querySelector('#note-title');
+		this.#$formButtons = document.querySelector('#form-buttons');
+
+		this.addEventListeners();
+	}
+
+	addEventListeners() {
+		document.body.addEventListener('click', (event) => {
+			this.handleFormClick(event);
+		});
+	}
+
+	handleFormClick(event) {
+		const isFormClicked = this.#$form.contains(event.target);
+
+		isFormClicked ? this.openForm() : '';
+	}
+
+	openForm() {
+		this.#$form.classList.add('form-open');
+		this.#$noteTitle.style.display = 'block';
+		this.#$formButtons.style.display = 'block';
+	}
 }
 
 new App();
