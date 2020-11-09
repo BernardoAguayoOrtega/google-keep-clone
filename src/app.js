92be6a3c387e81c5b55a85b32cc0ade07b44c4d1
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-09 11:42:50 -0600

    change private values for comun values

--- src/app.js
+++ src/app.js
@@ -1,45 +1,46 @@
 class App {
-	/**private variables */
-	#form;
-	#noteTitle;
-	#formButtons;
+  constructor() {
+    this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
+    this.$formButtons = document.querySelector("#form-buttons");
 
-	constructor() {
-		/** innit variables */
-		this.#form = document.querySelector('#form');
-		this.#noteTitle = document.querySelector('#note-title');
-		this.#formButtons = document.querySelector('#form-buttons');
+    this.addEventListeners();
+  }
 
-		this.addEventListeners();
-	}
+  addEventListeners() {
+    document.body.addEventListener("click", event => {
+      this.handleFormClick(event);
+    });
+    
+    this.$form.addEventListener('submit', event => {
+      event.preventDefault(); 
+      const title = this.$noteTitle.value;   
+      const text = this.$noteText.value;   
+    });
+  }
 
-	addEventListeners() {
-		document.body.addEventListener('click', (event) => {
-			this.handleFormClick(event);
-		});
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
 
-		this.#form.addEventListener('submit', (event) => {
-			event.preventDefault();
-		});
-	}
+    if (isFormClicked) {
+      this.openForm();
+    } else {
+      this.closeForm();
+    }
+  }
 
-	handleFormClick(event) {
-		const isFormClicked = this.#form.contains(event.target);
+  openForm() {
+    this.$form.classList.add("form-open");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
 
-		isFormClicked ? this.openForm() : this.closeForm();
-	}
-
-	openForm() {
-		this.#form.classList.add('form-open');
-		this.#noteTitle.style.display = 'block';
-		this.#formButtons.style.display = 'block';
-	}
-
-	closeForm() {
-		this.#form.classList.remove('form-open');
-		this.#noteTitle.style.display = 'none';
-		this.#formButtons.style.display = 'none';
-	}
+  closeForm() {
+    this.$form.classList.remove("form-open");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+  }
 }
 
 new App();
